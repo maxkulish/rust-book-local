254e2fc24c8ae053eceb1d69faf6e1cd3d140120
Max Kul <kma.memo@gmail.com>
2020-09-15 06:51:24 +0300

    chapter 01

---
+++ ch01/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/maxkul/Code/rust-book-local/ch01/hello_cargo/target/release/hello_cargo: /Users/maxkul/Code/rust-book-local/ch01/hello_cargo/src/main.rs
---
+++ ch01/hello_cargo/target/rls/debug/deps/hello_cargo-e986d652c62dffae.d
@@ -0,0 +1,5 @@
+/Users/maxkul/Code/rust-book-local/ch01/hello_cargo/target/rls/debug/deps/hello_cargo-e986d652c62dffae.rmeta: src/main.rs
+
+/Users/maxkul/Code/rust-book-local/ch01/hello_cargo/target/rls/debug/deps/hello_cargo-e986d652c62dffae.d: src/main.rs
+
+src/main.rs:
