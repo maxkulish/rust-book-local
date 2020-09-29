1d9de0a813e2eee39cf15288dd925d271574f901
Max Kul <kma.memo@gmail.com>
2020-09-29 08:40:53 +0300

    chapter 09

--- ch02/guessing_game/src/main.rs
+++ ch02/guessing_game/src/main.rs
@@ -18,12 +18,15 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
         
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-    
-        println!("You gessed: {}", guess);
+
+        if guess < 1 || guess > 100 {
+            println!("The secret number should be between 1 and 100");
+            continue;
+        }
     
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
