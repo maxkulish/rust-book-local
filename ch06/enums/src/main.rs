2d8cfd6c735216a5b603c879a093dce5d2f016e0
Max Kulish <kma.memo@gmail.com>
2022-07-09 16:01:09 +0300

    chapter 06

--- ch06/enums/src/main.rs
+++ ch06/enums/src/main.rs
@@ -7,4 +7,22 @@ fn main() {
     if let Some(3) = some_u8_value {
         println!("three");
     }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
