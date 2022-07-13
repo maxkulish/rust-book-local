721e2f4c065a3ad2a0a7e4ef65a3c56f661a71f2
Max Kulish <kma.memo@gmail.com>
2022-07-13 13:37:50 +0300

    chapter 08 - hash maps

--- ch08/hash_maps/src/main.rs
+++ ch08/hash_maps/src/main.rs
@@ -1,49 +1,40 @@
 use std::collections::HashMap;
 
 fn main() {
+
     let mut scores = HashMap::new();
 
     scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Blue"), 20);
+    scores.insert(String::from("Yellow"), 20);
 
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Yellow")).or_insert(60);
-
-    println!("{:?}", scores);
-
-    for (key, value) in &scores {
-        println!("{}: {}", key, value);
-    }
+    scores.insert(String::from("Yellow"), 25);
 
     let teams = vec![String::from("Blue"), String::from("Yellow")];
     let initial_scores = vec![10, 50];
 
-    let scores: HashMap<_, _> = 
-        teams.into_iter().zip(initial_scores.into_iter()).collect();
+    let mut scores: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
 
+    println!("Scores: {:?}", scores);
+
+    let team_name = String::from("Green");
+    let score = scores.get(&team_name);
+
+    match score {
+        Some(num) => println!("score: {}", num),
+        None => println!("There are no such team"),
+    }
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Red")).or_insert(100);
     println!("{:?}", scores);
 
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
-
-    let mut map = HashMap::new();
-    map.insert(field_name, field_value);
-
-    // We aren't able to use the variable field_name and field_value
-    // after they've been moved into the hash map
-    println!("{:?}", map);
-
-    let field_name = String::from("Favorite color");
-    println!("{:?}", map.get(&field_name));
-
-    let text = "hello world wonderful world hello mother";
+    let txt = "hello world wonderful world strange world";
 
     let mut map = HashMap::new();
 
-    for word in text.split_whitespace() {
+    for word in txt.split_whitespace() {
         let count = map.entry(word).or_insert(0);
         *count += 1;
     }
-
     println!("{:?}", map);
 }
