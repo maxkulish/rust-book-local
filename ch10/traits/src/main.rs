d4030778ac5315c115ea170be5a036be3b5dc36e
Max Kulish <kma.memo@gmail.com>
2022-07-30 17:22:38 +0300

    chapter 12 - minigrep

--- ch10/traits/src/main.rs
+++ ch10/traits/src/main.rs
@@ -1,72 +1,16 @@
-pub trait Summary {
-    fn summarize_author(&self) -> String;
+pub mod aggregator;
 
-    fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author())
-    }
-}
-
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {
-
-    fn summarize_author(&self) -> String {
-        format!("author: {}", self.author)
-    }
-
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
-}
-
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.username)
-    }
-
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
-}
-
-pub fn notify(item: &impl Summary) {
-    println!("Breaking news! {}", item.summarize());
-}
+use aggregator::{Summary, Tweet};
 
 fn main() {
     let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
+        username: String::from("Nestor"),
+        content: String::from(
+            "of course, this is my first text"
+        ),
         reply: false,
         retweet: false,
     };
 
-    println!("1 new tweet: {}", tweet.summarize());
-
-    let article = NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        location: String::from("Kyiv, Ukraine"),
-        author: String::from("Nestor Smirnoff"),
-        content: String::from(
-            "The Pitsburg Penguins once again are the best \
-             hockey tam in the NHL.",
-        ),
-    };
-
-    println!("New article available: {}", article.summarize());
-
-    notify(&article);
+    println!("1 new tweet: {}", tweet.summarize())
 }
