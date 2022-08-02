74416fae7c66686d623a6d3695865f389908443b
Max Kulish <kma.memo@gmail.com>
2022-08-02 12:07:31 +0300

    chapter 12 - minigrep optimized

--- ch12/mgrep/src/lib.rs
+++ ch12/mgrep/src/lib.rs
@@ -25,15 +25,21 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        } else if args.len() > 3 {
-            return Err("too many arguments");
-        }
+    pub fn new(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        // call next to skip the name of the program
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query= match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
